20256957b64d73b0a3825141b49429b059203899
rembrantiban <rembrantiban62@gmail.com>
2026-04-13 12:28:11 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import './App.css'
+import HomePage from "./pages/Homapage"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
