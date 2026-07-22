350a0245c82750ddb4f3ae667e530412937f8a31
rembrantiban <rembrantiban62@gmail.com>
2026-07-22 09:48:14 +0800

    Adding Url

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
   withCredentials: true,
 });
 
-export default AxiosInstance; 
+export default AxiosInstance;
